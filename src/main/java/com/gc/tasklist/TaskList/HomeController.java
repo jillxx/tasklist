263bad1b8eded436f792cbcd5a0c1ac2067d599c
jillxx <jiaoxu84@gmail.com>
2018-06-01 11:30:50 -0400

    update 2

--- src/main/java/com/gc/tasklist/TaskList/HomeController.java
+++ src/main/java/com/gc/tasklist/TaskList/HomeController.java
@@ -8,37 +8,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.gc.coffeeshop.entity.Items;
+import com.gc.tasklist.TaskList.dao.taskDao;
+import com.gc.tasklist.TaskList.dao.userDao;
+import com.gc.tasklist.TaskList.entity.Task;
+import com.gc.tasklist.TaskList.entity.User;
 
 @Controller
 public class HomeController {
 
 	@Autowired
-	private UserDao userDao;
-	private TaskDao taskDao;
-	
+	private userDao userDao;
+	private taskDao taskDao;
+
 	@RequestMapping("/")
 	public ModelAndView index() {
-		return new ModelAdnView ("index");
+		return new ModelAdnView("index");
 	}
-	
+
 	@RequestMapping("/preRegister")
 	public String form() {
 		return "register";
 	}
-	
+
 	@RequestMapping("/register")
 	public ModelAndView register(@RequestParam("email") String email, @RequestParam("password") String password) {
 		User user = new User(email, password);
 		userDao.addUser(user);
-		return new ModelAndView ("redirect:/");
+		return new ModelAndView("redirect:/");
 	}
-	
+
 	@RequestMapping("/login")
 	public ModelAndView login(@RequestParam("email") String email, @RequestParam("password") String password) {
-		List<Task> tasklist = taskDao.findList(email, password);
-		
-		return new ModelAdnView ("task", "tasklist", tasklist);
+		if (userDao.checkUser(email, password)) {
+				
+			List<Task> tasklist = taskDao.listTasks();
+
+			return new ModelAdnView("task", "tasklist", tasklist);
+		} else {
+				System.out.println("not match");
+			return new ModelAndView("index");
+		}
 	}
 
 	@RequestMapping("/addItem")
@@ -49,11 +58,10 @@ public class HomeController {
 	@RequestMapping("/add")
 	public ModelAndView add(@RequestParam("description") String description, @RequestParam("duedate") String duedate,
 			@RequestParam("status") String status) {
-		
+
 		Task task = new Task(description, duedate, status);
 		taskDao.update(task);
 		return new ModelAndView("index", "tasklist", taskDao.findlist());
 	}
 
-	
 }
